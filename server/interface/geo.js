402d08257082d5e66ebb75ef5450e4c5fd4d8c37
小白 <xiaobai@xiaobaideMacBook-Pro.local>
2018-11-21 09:49:15 +0800

    mt

---
+++ server/interface/geo.js
@@ -0,0 +1,84 @@
+import Router from 'koa-router'
+import axios from './utils/axios'
+
+let router = new Router({
+  prefix: '/geo'
+})
+
+const sign = 'b8ffc13a9e6b6014fef3ca4833ce488b'
+
+router.get('/getPosition', async (ctx) => {
+  let {status, data:{province,city}} = await axios.get(`http://cp-tools.cn/geo/getPosition?sign=${sign}`)
+  if (status === 200) {
+    ctx.body = {
+      province,
+      city
+    }
+  } else {
+    ctx.body = {
+      province: '',
+      city: ''
+    }
+  }
+})
+
+router.get('/province', async (ctx) => {
+  let { status, data: {province} } = await axios.get(`http://cp-tools.cn/geo/province?sign=${sign}`)
+  ctx.body = {
+    province: status === 200 ? province : []
+  }
+})
+
+router.get('/province/:id', async (ctx) => {
+  let { status, data: {city} } = await axios.get(`http://cp-tools.cn/geo/province/${ctx.params.id}?sign=${sign}`)
+  if (status === 200) {
+    ctx.body = {
+      city
+    }
+  } else {
+    ctx.body = {
+      city: []
+    }
+  }
+})
+
+router.get('/city', async (ctx) => {
+  let { status, data: {city} } = await axios.get(`http://cp-tools.cn/geo/city?sign=${sign}`)
+  if (status === 200) {
+    ctx.body = {
+      city
+    }
+  } else {
+    ctx.body = {
+      city: []
+    }
+  }
+})
+
+router.get('/hotCity', async (ctx) => {
+  let { status, data: {hots} } = await axios.get(`http://cp-tools.cn/geo/hotCity?sign=${sign}`)
+  if (status === 200) {
+    ctx.body = {
+      hots
+    }
+  } else {
+    ctx.body = {
+      hots: []
+    }
+  }
+})
+
+router.get('/menu', async (ctx) => {
+  let {status, data:{menu}} = await axios.get(`http://cp-tools.cn/geo/menu?sign=${sign}`)
+  if (status === 200) {
+    ctx.body = {
+      menu
+    }
+  } else {
+    ctx.body = {
+      menu: []
+    }
+  }
+})
+
+export default router
